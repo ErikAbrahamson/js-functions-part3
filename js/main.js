2772b7eeb5d76ad68f1dc2c2b90abc471aad7712
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-08-04 11:03:59 -0600

    Init main project files

---
+++ js/main.js
@@ -0,0 +1,38 @@
+(document).on('ready', function() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  module.exports = {
+    getStudentName: getStudentName,
+    getTotalLetters: getTotalLetters,
+    createObject: createObject,
+    getNegativeIndex: getNegativeIndex,
+    removeCharacter: removeCharacter,
+    outputObject: outputObject,
+    getVowels: getVowels,
+    captureTwins: captureTwins,
+    testBooleanLogic: testBooleanLogic,
+    getUniqueValues: getUniqueValues
+  };
+
+});
